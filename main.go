304df0007025c0f5b744a9b112cb1c70f7c75414
aaaasahi <asapjorf1017@gmail.com>
2024-06-02 21:14:42 +0900

    リファクタ

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
-	"time"
 
 	"go-clean-app/config"
 	"go-clean-app/server"
 	"go-clean-app/di"
+	"go-clean-app/infra/mysql/db"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -19,27 +17,13 @@ func main() {
 		log.Fatal(err)
 	} 
 
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", c.DB.User, c.DB.Password, c.DB.Host, c.DB.Port, c.DB.Name)
-
-	var db *sql.DB
-	var err error
-	for i := 0; i < 10; i++ {
-		db, err = sql.Open("mysql", dataSourceName)
-		if err == nil {
-			err = db.Ping()
-			if err == nil {
-				break
-			}
-		}
-		log.Printf("Waiting for database to be ready... (%d/10)\n", i+1)
-		time.Sleep(5 * time.Second)
-	}
+	dbConn, err := db.Connect(c)
 	if err != nil {
-		log.Fatal("Failed to connect to the database:", err)
+		log.Fatalf("Failed to connect to the database: %v", err)
 	}
-	defer db.Close()
+	defer dbConn.Close()
 
-	container := di.BuildContainer(db)
+	container := di.BuildContainer(dbConn)
 
 	if err := container.Invoke(func(s *server.Server) {
 		s.Run(&c)
