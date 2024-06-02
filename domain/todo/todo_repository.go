69d8fdf38785bd7df2329956d1aad0fc753a4bad
aaaasahi <asapjorf1017@gmail.com>
2024-06-02 15:33:28 +0900

    domainのrepositoryでinterfaceを定義

---
+++ domain/todo/todo_repository.go
@@ -0,0 +1,9 @@
+package todo
+
+import "context"
+
+type TodoRepository interface {
+	Save(ctx context.Context, todo *Todo) error
+	FindById(ctx context.Context, id int64) (*Todo, error)
+	FindAll(ctx context.Context) ([]*Todo, error)
+}
